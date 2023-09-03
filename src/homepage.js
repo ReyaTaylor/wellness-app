f1e5e58f7fe2fca709decd041553d022d904fb75
Reya Taylor <adamsreya@gmail.com>
2023-09-03 07:42:58 -0400

    adding project to github

---
+++ src/homepage.js
@@ -0,0 +1,14 @@
+import React from "react"; 
+
+function Homepage() {
+    return (
+        <div>
+            <p>DASHBOARD</p>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Homepage;
---
+++ src/workouts.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Workouts() {
+    return (
+        <div>
+            <p>This is my wellness App</p>
+        </div>
+    )
+}
+
+export default Workouts;
